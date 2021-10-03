96a2c97e7ca1ee2e28f0a6edc6cb567e1acffeed
phillipprudencio <phillip_prudencio@hotmail.com>
2021-10-03 17:59:51 -0300

    Alteração elb

--- load_balancer.tf
+++ load_balancer.tf
@@ -1,3 +1,16 @@
+resource "aws_lb" "ecs-elb" {
+  name               = "ecs-elb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = aws_subnet.public.*.id
+
+  enable_deletion_protection = no
+ 
+  tags = {
+    Environment = "production"
+  }
+}
 resource "aws_lb_listener_rule" "main" {
   count = var.no_loadbalancer ? 0 : 1
   listener_arn = var.loadbalancer_rule_arn
