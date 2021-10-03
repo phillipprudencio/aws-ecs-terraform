38a641fb0b1ebbf3804c051a1534839af963b5c3
phillipprudencio <phillip_prudencio@hotmail.com>
2021-10-03 19:22:31 -0300

    Main

--- load_balancer.tf
+++ load_balancer.tf
@@ -1,5 +1,5 @@
 resource "aws_lb" "ecs-elb" {
-  name               = "ecs-elb-tf"
+  name               = "ecs-elb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = ["sg-02041171d46e310ad"]
@@ -9,6 +9,7 @@ resource "aws_lb" "ecs-elb" {
     Environment = "production"
   }
 }
+
 resource "aws_lb_listener_rule" "main" {
   count = var.no_loadbalancer ? 0 : 1
   listener_arn = var.loadbalancer_rule_arn
