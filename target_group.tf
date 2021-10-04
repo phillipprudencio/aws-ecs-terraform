6fa94b3c1cad400419deffb2a64fcace809670d6
phillipprudencio <82064589+phillipprudencio@users.noreply.github.com>
2021-10-04 18:33:57 -0300

    Update target_group.tf

--- target_group.tf
+++ target_group.tf
@@ -1,8 +1,22 @@
-resource "aws_lb_target_group" "main" {
-  name        = "lb-target-group"
+
+resource "aws_alb_target_group" "main" {
+  count       = var.no_loadbalancer ? 0 : 1
   port        = var.task_port
   protocol    = var.healthcheck_protocol
   vpc_id      = var.vpc_id
   target_type = "ip"
   tags        = var.tags
+
+  health_check {
+    interval            = var.healthcheck_interval
+    path                = var.healthcheck_path
+    timeout             = var.healthcheck_timeout
+    matcher             = var.healthcheck_success_codes
+    healthy_threshold   = var.healthcheck_healthy_threshold
+    unhealthy_threshold = var.healthcheck_unhealthy_threshold
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
