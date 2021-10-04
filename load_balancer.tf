859c7f8877ff3053c8d94ed915ee007a6f974542
phillipprudencio <82064589+phillipprudencio@users.noreply.github.com>
2021-10-04 13:49:31 -0300

    Update load_balancer.tf

--- load_balancer.tf
+++ load_balancer.tf
@@ -16,6 +16,6 @@ resource "aws_lb_listener" "web-listener" {
   protocol          = "HTTP"
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.lb_target_group.arn
+    target_group_arn = aws_lb_target_group.lb-target-group.arn
   }
 }
