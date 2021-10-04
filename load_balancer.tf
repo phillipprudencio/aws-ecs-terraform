46725747a31f25672aa64a6a0394f2f8eeaccd8e
phillipprudencio <82064589+phillipprudencio@users.noreply.github.com>
2021-10-04 14:15:58 -0300

    Update load_balancer.tf

--- load_balancer.tf
+++ load_balancer.tf
@@ -5,9 +5,23 @@ resource "aws_lb" "ecs-elb" {
   security_groups    = ["sg-02041171d46e310ad"]
   subnets               = ["subnet-e5f594be", "subnet-0830be6d"] 
 
+}
+
+
+resource "aws_lb_listener_rule" "main" {
+  count = var.no_loadbalancer ? 0 : 1
+  listener_arn = aws_alb_target_group.arn
+
   action {
     type             = "forward"
     target_group_arn = aws_alb_target_group.main[0].arn
   }
 
- }
+  condition {
+    host_header {
+      values = [
+        var.hostname
+      ]
+    }
+  }
+}
