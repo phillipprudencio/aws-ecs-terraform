533d50fb3f7829388a03d182099b92214167d3b3
phillipprudencio <82064589+phillipprudencio@users.noreply.github.com>
2021-10-04 10:30:42 -0300

    Update load_balancer.tf

--- load_balancer.tf
+++ load_balancer.tf
@@ -1,4 +1,4 @@
-resource "aws_lb" "ecs-elb" {
+./ resource "aws_lb" "ecs-elb" {
   name               = "ecs-elb"
   internal           = false
   load_balancer_type = "application"
@@ -27,3 +27,55 @@ resource "aws_lb_listener_rule" "main" {
     }
   }
 }
+
+./
+  resource "aws_lb" "ecs-elb" {
+  name               = "ecs-elb"
+  load_balancer_type = "application"
+  internal           = false
+  subnets            = ["subnet-e5f594be", "subnet-0830be6d"]
+  tags = {
+    "env"       = "dev"
+    "createdBy" = "mkerimova"
+  }
+  security_groups = [aws_security_group.lb.id]
+}
+resource "aws_security_group" "lb" {
+  name   = "allow-all-lb"
+  vpc_id = "vpc-aafc48cc"
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    "env"       = "dev"
+    "createdBy" = "mkerimova"
+  }
+}
+
+resource "aws_lb_listener_rule" "main" {
+  count = var.no_loadbalancer ? 0 : 1
+  listener_arn = var.loadbalancer_rule_arn
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.main[0].arn
+  }
+
+  condition {
+    host_header {
+      values = [
+        var.hostname
+      ]
+    }
+  }
+}
+
