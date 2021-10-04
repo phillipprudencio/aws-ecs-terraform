16d4b5d7deeb5d9a347a0d391d3a67684697ef92
phillipprudencio <82064589+phillipprudencio@users.noreply.github.com>
2021-10-04 14:05:20 -0300

    Update target_group.tf

--- target_group.tf
+++ target_group.tf
@@ -1,6 +1,5 @@
 resource "aws_lb_target_group" "main" {
   name        = "lb-target-group"
-  count       = var.no_loadbalancer ? 0 : 1
   port        = var.task_port
   protocol    = var.healthcheck_protocol
   vpc_id      = var.vpc_id
